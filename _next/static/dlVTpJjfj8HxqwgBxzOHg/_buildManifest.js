44dffd1a62739f2c3362a7d9823c9aa9c917202a
astongemmy <52787496+astongemmy@users.noreply.github.com>
2022-11-30 18:08:07 +0000

    Build: (8b3e964) Added new build workflow

--- _next/static/dlVTpJjfj8HxqwgBxzOHg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8f8e2a675a5c20da7eb7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b254ceb51bbc5b159c80.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
