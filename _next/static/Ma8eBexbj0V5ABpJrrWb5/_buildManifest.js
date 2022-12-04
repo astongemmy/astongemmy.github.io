59b0b0383fc85e40cf6386edf2591563da6ee690
astongemmy <52787496+astongemmy@users.noreply.github.com>
2022-12-04 11:28:01 +0000

    Build: (fcb3c69) Added new line edits to About section

--- _next/static/Ma8eBexbj0V5ABpJrrWb5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0e5864680b89237f1e6d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6b231ba5e1933bb368cd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
