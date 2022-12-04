8c87679fa2479a7c0038e4d864424c89fc5f0432
astongemmy <52787496+astongemmy@users.noreply.github.com>
2022-12-04 15:12:27 +0000

    Build: (c75d438) General refactoring

---
+++ _next/static/Hy4UGa2W8hfWILi8-WVL2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a4d93c9da47a454a5e4b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6b231ba5e1933bb368cd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/kUG99z8ST5mD848-0GpjB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-94d5c6b0c7e189cc2c9e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6b231ba5e1933bb368cd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
